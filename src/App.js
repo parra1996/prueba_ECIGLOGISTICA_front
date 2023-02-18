14d5d39e4721c5d33c9559ea60b392a792f46bc8
“juanP1996” <parrajp1996@gmail.com>
2023-02-18 14:09:43 +0100

    feat: containers y dependencias creadas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './containers/home/home';
+import Login from './containers/login/login';
+import Register from './containers/register/register'
 
 function App() {
   return (
     <div className="App">
-      somos los que somos
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+        </Routes>
+      </BrowserRouter>
+
+      
     </div>
   );
 }
